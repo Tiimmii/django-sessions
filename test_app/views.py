d9db7216c03abcee95d68c5c7f87f932edf46fff
Timmi <danieloluwoleoluwatimileyin@gmail.com>
2023-04-18 10:23:36 +0100

    first-commit

---
+++ test_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views import View, generic
+from .models import Product
+import requests
+# Create your views here.
+class product_list(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'product'
+
+    def load_products(request):
+        r = requests.get('https://fakestoreapi.com/products')
+        for i in r.json():
+            product = Product(
+                title = i['title'],
+                description = i['description'],
+                price = i['price'],
+                image_url = i['image']
+            )
+            product.save()
+            
+    def get_queryset(self):
+        return Product.objects.all()
+    
+
